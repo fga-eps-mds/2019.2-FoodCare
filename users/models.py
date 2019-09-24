a8702c6111787e072b51b89e0b95dcb930a9451f
Lucas Lopes <l.lopes.fga@gmail.com>
2019-09-23 23:50:57 -0300

    Adiciona model Usuario
    Co-authored-by: RafaellaJunqueira <rafaellafjunqueira@gmail.com>

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from operator import itemgetter
 # Create your models here.
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cnpj = models.CharField(max_length=14,blank=True)
+
+    
+    def __str__(self):
+        return self.user.username
+    
+    @receiver(post_save, sender=User)
+    def create_user_usuario(sender, instance, created, **kwargs):
+        if created:
+            Usuario.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_usuario(sender, instance, **kwargs):
+        instance.usuario.save()
