f8a88e8e714ac1068c24d59fa0c2e7894a367646
William Almeida <will.allmeida@gmail.com>
2019-09-25 00:08:33 -0300

    Arruma travis para ignorar docs

--- foodcare/urls.py
+++ foodcare/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path, include
 from rest_framework import routers
-from user import views
 
 router = routers.DefaultRouter()
 #router.register(r'users', views.UserViewSet)
