de22b5fe46d2c7cfa9c64ca27bbac0f7b9f15b89
codehg <hugoaraliveira@gmail.com>
2019-09-27 15:45:12 -0300

    Adiciona serializer
    Co-authored-by: GiovannaBot <gibottino@gmail.com>

--- evento/views.py
+++ evento/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Evento
+from .serializers import EventoSerializer
 
-# Create your views here.
+
+class EventoViewSet(viewsets.ModelViewSet):
+
+    serializer_class = EventoSerializer
+    queryset = Evento.objects.all()
+
--- foodcare/urls.py
+++ foodcare/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from evento.views import EventoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(
+    'evento', EventoViewSet, base_name='evento'
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ]
