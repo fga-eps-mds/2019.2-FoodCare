40051401c403c3b434ea95cd4712c766c396c598
Geraldo Victor <geraldovictor@outlook.com>
2019-10-01 20:42:27 -0300

    Adiciona chave estrangeira de eventos em cada alimento e campo da localidade

--- evento/models.py
+++ evento/models.py
@@ -6,18 +6,20 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=60)
     
 
+class Evento(models.Model):
+    id = models.AutoField(primary_key=True)
+    #id_doador = models.ForeignKey("Doador", on_delete=models.CASCADE, related_name = 'evento')
+    #id_endereco = models.ForeignKey("Endereco", on_delete=models.CASCADE, related_name = 'evento')
+    data_inicio = models.DateTimeField(default=datetime.now)
+    data_final = models.DateTimeField(default=datetime.now)
+    local = models.CharField(max_length=50, blank=False)
+
 class Alimento(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=60)
     un_medida = models.CharField(max_length=60)
     quantidade = models.IntegerField()
     id_categoria = models.ManyToManyField(Categoria)
-    
+    id_evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
 
-class Evento(models.Model):
-    id = models.AutoField(primary_key=True)
-    #id_doador = models.ForeignKey("Doador", on_delete=models.CASCADE, related_name = 'evento')
-    #id_endereco = models.ForeignKey("Endereco", on_delete=models.CASCADE, related_name = 'evento')
-    id_alimento = models.ManyToManyField(Alimento)
-    data_inicio = models.DateTimeField(default=datetime.now)
-    data_final = models.DateTimeField(default=datetime.now)
+    
