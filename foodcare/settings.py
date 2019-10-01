d0bf63838857c3aa81e9a6323330fbf433237f55
Lucas Lopes <l.lopes.fga@gmail.com>
2019-10-01 00:31:11 -0300

    Atualizaçoes
    Co-authored-by: RafaellaJunqueira <rafaellafjunqueira@gmail.com>

--- foodcare/settings.py
+++ foodcare/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'doadores',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'foodcare.urls'
@@ -120,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:4200',
+)
