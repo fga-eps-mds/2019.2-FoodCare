ebd9d83ed9279ae2fc226eb875e146e58d339952
Lucas Lopes <l.lopes.fga@gmail.com>
2019-10-12 20:57:23 -0300

    Configurado o recebimento de email
    Co-authored-by: brunaalmeidasantos <brunaalmeida48@gmail.com>
    Signed-off-by: Lucas Lopes <l.lopes.fga@gmail.com>

--- emailfood/views.py
+++ emailfood/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render,redirect
 from django.core.mail import send_mail
-
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+
+import json
 
 # Create your views here.
 def mensagem(nome,email,mensagem):
     msg = """
 
-    Olá, {}
-    {}    
-    {}
+    Nome: {}
+    Mensagem: {}    
+    Email: {}
     """.format(nome,mensagem,email)
     return msg
 
 def index(n,e,m):
-    subject = "FoodCare: Mensagem de {}".format(n)
+    subject = "FoodCare: {}".format(n)
     message = mensagem(n,e,m)
     email_from = settings.EMAIL_HOST_USER
     recipient_list = [settings.EMAIL_HOST_USER,]
@@ -24,7 +26,10 @@ def index(n,e,m):
 @csrf_exempt
 def email(request):
     if request.method == "POST":
-        print(request.POST)
-        index(request.POST.get('nome'),request.POST.get('email'),request.POST.get('msg'))
-        return redirect('/admin/')
-    return redirect('/admin/')
+        body_unicode = request.body.decode('utf-8')
+        resposta = json.loads(body_unicode)
+        print(resposta)
+        index(resposta["nome"],resposta["email"],resposta["msg"])
+        return HttpResponse(status=204)
+    
+    
