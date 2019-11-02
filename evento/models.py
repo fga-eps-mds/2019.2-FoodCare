dbcbb0aab2628611396f7a5e88e38c694c4fe048
GiovannaBot <gibottino@gmail.com>
2019-11-01 23:04:24 -0300

    Adiciona teste unitario em evento

--- evento/models.py
+++ evento/models.py
@@ -3,12 +3,26 @@ from datetime import datetime, date
 from users.models import Usuario
 
 class Evento(models.Model):
+    """
+    # Cria um evento
+    >>> evento = EventoEvento.objects.create(
+            nome="Teste",
+            id_doador= id_doador, 
+            data_inicio=datetime.now(), 
+            data_final=datetime.now(), 
+            desc="Apenas um teste"
+            )
+
+    # Fala o nome
+    >>> evento.getNome()
+    'Teste'
+    """
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=60, blank=False)
     id_doador = models.ForeignKey('users.Usuario', on_delete=models.CASCADE)
     data_inicio = models.DateTimeField(default=datetime.now)
     data_final = models.DateTimeField(default=datetime.now)
-    # local = models.CharField(max_length=50, blank=False)
     desc = models.TextField(max_length=300, blank=False)
-    # doador  = models.ForeignKey(Doadores,on_delete=models.CASCADE)
     
+    def getID(self):
+        return self.id_doador
--- evento/tests.py
+++ evento/tests.py
@@ -1,11 +1,24 @@
 from django.test import TestCase
-from .models  import Evento
+from .models  import Evento 
 from users.models import Usuario
+from datetime import datetime, date
+import unittest
 
-class Usuario(TestCase):
-    def setup(self):
-        Usuario.objects.create(username="Fulano",cnpj="123456789-0000")
-    def test_create_user(self):
-        fulano = Usuario.objects.get(username="Fulano")
-        self.assertEquals(fulano.username,"Fulano")
-        self.assertEquals(fulano.cnpj,"123456789-0000")
+class EventoTest(unittest.TestCase):
+    def setUp(self):
+        # o id do doador nao ta funcionando corretamente, o restante esta
+        doador = Usuario.objects.create(user="Fulano", cnpj="12345678901234")
+        id_doador = doador.id
+
+        self.evento = Evento.objects.create(
+            nome="Teste",
+            id_doador= id_doador, 
+            data_inicio=datetime.now(), 
+            data_final=datetime.now(), 
+            desc="Apenas um teste"
+            )
+
+    def test_criacao_evento(self):
+        print("Testando criacao de evento")
+        self.assertEqual(self.evento.getID(), "Teste")
+        print("Criacao de evento testado")
