54ccc1c047a790368cd18d1537b50407a453036f
GiovannaBot <gibottino@gmail.com>
2019-11-03 18:35:47 -0300

    Adiciona uls para users
    Co-authored-by: LucasPLopes <l.lopes.fga@gmail.com>

--- foodcare/urls.py
+++ foodcare/urls.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
-from evento.views import EventoViewSet
+
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 from evento.views import EventoViewSet
+from users.views import UsuarioSerializerViewSet
 
 router = routers.DefaultRouter()
 router.register(
     'evento', EventoViewSet, base_name='evento'
+)
+
+router2 = routers.DefaultRouter()
+router2.register(
+    'user', UsuarioSerializerViewSet, base_name='usuario'
 )
 
 
@@ -21,5 +27,6 @@ urlpatterns = [
     path('auth/refresh-token/', refresh_jwt_token),
     path('',include('emailfood.urls')),
     path('', include(router.urls)),
+    path('', include(router2.urls)),
 
 ]
