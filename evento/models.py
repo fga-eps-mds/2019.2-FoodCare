d597fca12bf70afad0884cfc3716686b8ef7782e
GiovannaBot <gibottino@gmail.com>
2019-11-05 12:03:18 -0300

    Adiciona categoria
    Co-authored-by: brunaalmeidasantos <brunaalmeida48@gmail.com>

--- evento/models.py
+++ evento/models.py
@@ -14,3 +14,11 @@ class Evento(models.Model):
 
     def __str__(self):
         return "{} {}".format(self.nome, self.id_doador.id)
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=60, blank=False) 
+
+    def __str__(self):
+        return "{}".format(self.nome)
+
--- evento/views.py
+++ evento/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
-from .models import Evento
-from .serializers import EventoSerializer
+from .models import Evento, Categoria
+from .serializers import EventoSerializer, CategoriaSerializer
 
 
 class EventoViewSet(viewsets.ModelViewSet):
 
     serializer_class = EventoSerializer
     queryset = Evento.objects.all()
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CategoriaSerializer
+    queryset = Categoria.objects.all()
