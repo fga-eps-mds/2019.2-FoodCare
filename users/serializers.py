4de0851c4d43753f106ba72485faf3bf289a337c
codehg <hugoaraliveira@gmail.com>
2019-11-16 21:40:10 -0300

    Adiciona rota de user para feature delete
    Co-authored-by: geraldovictor <geraldovictor@outlook.com>

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from .models import Usuario
+from django.contrib.auth.models import User
 
 class UsuarioSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Usuario
         fields = '__all__'
-        
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
