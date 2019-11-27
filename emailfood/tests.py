7f89fe0848dd19d2fe9608fa380d953015f335e6
codehg <hugoaraliveira@gmail.com>
2019-11-27 17:02:07 -0300

    Testa index da views
    Co-authored-by: GiovannaBot <gibottino@gmail.com>

--- emailfood/tests.py
+++ emailfood/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from .views import mensagem
+from .views import mensagem, index, email
+from django.conf import settings
+from django.core.mail import send_mail
+
 
 class TestaEmail(TestCase):
     def testa_mensagem(self):
@@ -12,3 +15,14 @@ class TestaEmail(TestCase):
         msg_gabarito = "\n\nNome: Hugo Aragao\nMensagem: Teste de mensagem\nEmail: hugo@gmail.com\n"
 
         self.assertEqual(msg_teste, msg_gabarito)
+    
+    def testa_index(self):
+        teste_mail = send_mail("FoodCare: Hugo Aragao",
+        "\n\nNome: Hugo Aragao\nMensagem: Teste de mensagem\nEmail: hugo@gmail.com\n",
+        "foodcare.unb@gmail.com",
+        ['teste@gmail.com'],
+        fail_silently = False)
+
+        self.assertEquals(teste_mail, 1)
+          
+
